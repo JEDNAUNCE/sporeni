3ffbc094737b488e1e60235f6931b137986d72d5
JEDNAUNCE <v.ketner@seznam.cz>
2025-07-15 14:45:16 +0200

    Update smartemailing.js

--- api/smartemailing.js
+++ api/smartemailing.js
@@ -18,16 +18,15 @@ export default async function handler(req, res) {
 
   const credentials = Buffer.from(`${username}:${token}`).toString('base64');
 
-const payload = {
-  emailaddress: email,
-  name: `${jmeno} ${prijmeni}`,
-  customFields: {
-    telefon,
-    projekt: "sporeni"
-  },
-  groups: [19],
-  force_subscribe: true
-};
+  const payload = {
+    emailaddress: email,
+    name: `${jmeno} ${prijmeni}`,
+    customFields: {
+      telefon,
+      projekt: "sporeni"
+    },
+    force_subscribe: true
+  };
 
   console.log("📤 Odesílám do SmartEmailingu:", payload);
 
